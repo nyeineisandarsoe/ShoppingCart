c975daec913dff64d521c92babeabe1c5bd6066b
wang shuo <wangshuoliang@hotmail.com>
2019-04-02 10:39:05 +0800

    Add New Controller

---
+++ ShoppingCart/Controllers/ActiCodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    public class ActiCodeController : Controller
+    {
+        // GET: ActiCode
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
