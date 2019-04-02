2135438dbfdaa9f72ef0f50c2a489767465977fe
Thant Nay Lin <thantnaycode@gmail.com>
2019-04-02 10:42:40 +0800

    Added User Model

--- ShoppingCart/Models/User.cs
+++ ShoppingCart/Models/User.cs
@@ -7,5 +7,10 @@ namespace ShoppingCart.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
