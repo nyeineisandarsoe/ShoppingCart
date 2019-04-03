13f755f11ba7947372a79dc69c4f4b88915b6417
Thant Nay Lin <thantnaycode@gmail.com>
2019-04-03 15:37:07 +0800

    Updating Auth

--- ShoppingCart/Controllers/LoginController.cs
+++ ShoppingCart/Controllers/LoginController.cs
@@ -9,14 +9,37 @@ namespace ShoppingCart.Controllers
 {
     public class LoginController : Controller
     {
-        public ActionResult Main()
+        public ActionResult Index()
         {
             return View();
         }
 
        public ActionResult Authenticate(string Username, string Password)
         {
-            return View();
+            int user_id = 0;
+            string username = "";
+
+            List<User> user_info = new List<User>();
+
+            User user = new User();
+
+            user_info = user.validateUser(Username, Password);
+
+            foreach (var info in user_info)
+            {
+                user_id = info.UserId;
+                username = info.UserName;
+            }
+
+            if (user_id != 0)
+            {
+                Session["UserId"] = user_id;
+                Session["UserName"] = username;
+
+                return RedirectToAction("Index", "Product");
+            }
+
+            return RedirectToAction("Index", "Login");           
         }
 
         public ActionResult Logout(string sessionId)
